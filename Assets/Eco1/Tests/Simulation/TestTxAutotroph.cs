540fa9843b1b9874dd9648f06e99e1d2fd950e4c
Grant Morgan <grant@ryuuguu.com>
2020-02-06 15:12:23 +0900

    grow test,  grow bug

--- Assets/Eco1/Tests/Simulation/TestTxAutotroph.cs
+++ Assets/Eco1/Tests/Simulation/TestTxAutotroph.cs
@@ -41,8 +41,13 @@ namespace Tests {
             m_Manager.AddComponentData(stem, new Translation{ Value = float3.zero});
             m_Manager.AddComponentData(stem, new Shade{ Value = 0});
             m_Manager.AddComponentData(stem, new EnergyStore{ Value = 0});
-            m_Manager.AddComponentData(stem, new TxAutotrophParts {
-            });
+            m_Manager.AddComponentData(stem, new TxAutotrophParts {});
+            m_Manager.AddComponentData(stem, new PhysicsCollider {
+                    Value = Unity.Physics.SphereCollider.Create(
+                        new SphereGeometry {
+                            Center = float3.zero,
+                            Radius = 1,
+                        }, CollisionFilter.Default,new Unity.Physics.Material{Flags = Unity.Physics.Material.MaterialFlags.IsTrigger})});
 
             
             //physics collider
@@ -50,23 +55,28 @@ namespace Tests {
             m_Manager.AddComponentData(stem, new  TxAutotrophGenome {
             });
             m_Manager.AddComponentData(stem, new  TxAutotrophPhenotype {
-                  
+                leaf = 1,
+                height = 1,
+                seed =  0,
+                age = 0
             });
             
             
             sprout = m_Manager.CreateEntity();
             m_Manager.AddComponentData(sprout, new RandomComponent {random = new Unity.Mathematics.Random(1)});
             m_Manager.AddComponentData(sprout, new TxAutotrophSprout {location = new float3(1,2,3),energy = 5});
-            m_Manager.AddComponentData(sprout, new  TxAutotrophGenome {
-                  
-            });
+            m_Manager.AddComponentData(sprout, new  TxAutotrophGenome { });
             
+            
+            var es = new Environment.EnvironmentSettings[1]; 
+            Environment.environmentSettings = new NativeArray<Environment.EnvironmentSettings>(es,Allocator.Persistent);
 
         }
 
         [TearDown]
         public override void TearDown() {
             base.TearDown();
+            Environment.environmentSettings.Dispose();
         }
 
         [Test]
@@ -74,25 +84,92 @@ namespace Tests {
             World.CreateSystem<TxAutotrophSproutSystem>().Update(); 
             World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>().Update();
             
-            var sprouts = m_Manager.CreateEntityQuery(ComponentType.ReadWrite<RandomComponent>(),
+            var sprouts = m_Manager.CreateEntityQuery(ComponentType.ReadOnly<RandomComponent>(),
                 ComponentType.ReadOnly<TxAutotrophSprout>(),
                 ComponentType.ReadOnly<TxAutotrophGenome>()
             ).ToEntityArray(Allocator.TempJob);
             Assert.AreEqual(0,sprouts.Length,"Sprouts count");
             sprouts.Dispose();
             
-            //entity query to count TxAutotroph == 1
-            //entity query to count TxAutotrophLeafMeshFlag == 1
+            var stems = m_Manager.CreateEntityQuery(ComponentType.ReadOnly<TxAutotroph>(),
+                ComponentType.ReadOnly<TxAutotrophParts>()
+            ).ToEntityArray(Allocator.TempJob);
+            Assert.AreEqual(1,stems.Length,"Stems count");
             
+            var leafs = m_Manager.CreateEntityQuery(ComponentType.ReadOnly<TxAutotrophLeafMeshFlag>()
+            ).ToEntityArray(Allocator.TempJob);
+            Assert.AreEqual(1,leafs.Length,"Leafs count");
 
-            //check stem part == leaf exist and has TxAutotrophLeafMeshFlag
-
-
+            var e1 = m_Manager.GetComponentData<TxAutotrophParts>(stems[0]).leaf;
+            var e2 = leafs[0];
+            Assert.AreEqual(e1,e2,"stem.part.leaf == leaf");
+            
+            stems.Dispose();
+            leafs.Dispose();
         }
 
+        [Test]
+        public void TxAutotrophGrow_Test() {
+            //set Environment & genome 
+            m_Manager.SetComponentData(sprout, new TxAutotrophGenome {
+                nrg2Height = 1,
+                nrg2Leaf = 1,
+                nrg2Seed = 1,
+                nrg2Storage = 1,
+                seedSize = 4,
+                maxHeight = 5,
+                maxLeaf = 5.5f
+                
+            });
+            var es = Environment.environmentSettings[0];
+            es.txAutotrophConsts.seedDivisor = 2;
+            es.txAutotrophConsts.stemScale = 1;
+            Environment.environmentSettings[0] = es;
+            
+            World.CreateSystem<TxAutotrophSproutSystem>().Update(); 
+            World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>().Update();
 
-    /*
-  
+            var stems = m_Manager.CreateEntityQuery(ComponentType.ReadOnly<TxAutotroph>(),
+                ComponentType.ReadOnly<TxAutotrophParts>(),
+                ComponentType.ReadOnly<EnergyStore>(),
+                ComponentType.ReadOnly<TxAutotrophPhenotype>(),
+                ComponentType.ReadOnly<Scale>(),
+                ComponentType.ReadOnly<Translation>()
+                
+            ).ToEntityArray(Allocator.TempJob);
+
+            var plant = stems[0];
+            stems.Dispose();
+            
+            m_Manager.SetComponentData(plant, new EnergyStore(){Value = 10});
+            World.CreateSystem<TxAutotrophGrow>().Update();
+            var energy = m_Manager.GetComponentData<EnergyStore>(plant).Value;
+            Assert.AreEqual( 2.5f , m_Manager.GetComponentData<EnergyStore>(plant).Value,
+                "EnergyStore");
+            Assert.AreEqual( 3.5f , m_Manager.GetComponentData<TxAutotrophPhenotype>(plant).height,
+                "Height");
+            Assert.AreEqual( 3.5f , m_Manager.GetComponentData<TxAutotrophPhenotype>(plant).leaf,
+                "Leaf");
+            Assert.AreEqual( 1.25f , m_Manager.GetComponentData<TxAutotrophPhenotype>(plant).seed,
+                "Seed");
+            World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>().Update();
+            Assert.AreEqual(3.5f, m_Manager.GetComponentData<Scale>(plant).Value, "stem Scale");
+            
+            //second pass catches missing m_EndSimulationEcbSystem.AddJobHandleForProducer(jobHandle)in TxAutotrophGrow
+            m_Manager.SetComponentData(plant, new EnergyStore(){Value = 10f});
+            World.CreateSystem<TxAutotrophGrow>().Update();
+            World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>().Update();
+            Assert.AreEqual( 6f , m_Manager.GetComponentData<TxAutotrophPhenotype>(plant).height,
+                "Height Max");
+            Assert.AreEqual( 6f, m_Manager.GetComponentData<Scale>(plant).Value,
+                "stem Scale Max");
+            Assert.AreEqual( 5.5f , m_Manager.GetComponentData<TxAutotrophPhenotype>(plant).leaf,
+                "leaf Max");
+                Assert.AreEqual( 1f , m_Manager.GetComponentData<TxAutotrophPhenotype>(plant).seed,
+                "seed size ");
+        }
+    
+        /*
        // [Test]
        // public void TxAutotrophLight_Test() {
        //     World.CreateSystem<TxAutotrophLight>().Update();
@@ -206,31 +283,11 @@ namespace Tests {
                 "EnergyStore leafMultiple");
 
         }
-        [Test]
-        public void TxAutotrophGrow_Test() {
-            m_Manager.SetComponentData<EnergyStore>(plant, new EnergyStore(){Value = 10});
-            World.CreateSystem<TxAutotrophGrow>().Update();
-            var energy = m_Manager.GetComponentData<EnergyStore>(plant).Value;
-            Assert.AreEqual( 2.5f , m_Manager.GetComponentData<EnergyStore>(plant).Value,
-                "EnergyStore");
-            Assert.AreEqual( 3.5f , m_Manager.GetComponentData<TxAutotrophPhenotype>(plant).height,
-                "Height");
-            Assert.AreEqual( 3.5f , m_Manager.GetComponentData<TxAutotrophPhenotype>(plant).leaf,
-                "Leaf");
-            Assert.AreEqual( 2.5f , m_Manager.GetComponentData<TxAutotrophPhenotype>(plant).seed,
-                "Seed");
-            World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>().Update();
-            Assert.AreEqual(3.5f, m_Manager.GetComponentData<Scale>(stem).Value, "stem Scale");
-            
-            //second pass catches missing m_EndSimulationEcbSystem.AddJobHandleForProducer(jobHandle)in TxAutotrophGrow
-            m_Manager.SetComponentData<EnergyStore>(plant, new EnergyStore(){Value = 7.5f});
-            World.CreateSystem<TxAutotrophGrow>().Update();
-            World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>().Update();
-            Assert.AreEqual( 5.375f, m_Manager.GetComponentData<Scale>(stem).Value,
-                "stem Scale B");
-        }
+        
+        */
+        
 
-*/
+
     }
 
 
