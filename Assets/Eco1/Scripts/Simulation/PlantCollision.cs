dd28de40fbf4cc2f36b9d33f9d776af209473dd8
Grant Morgan <grant@ryuuguu.com>
2020-02-12 15:08:36 +0900

    starting on Fertilization code

--- Assets/Eco1/Scripts/Simulation/PlantCollision.cs
+++ Assets/Eco1/Scripts/Simulation/PlantCollision.cs
@@ -21,17 +21,12 @@ public class TriggerLeafSystem : JobComponentSystem {
 
     BuildPhysicsWorld m_BuildPhysicsWorldSystem;
     StepPhysicsWorld m_StepPhysicsWorldSystem;
-
-    NativeArray<int> m_TriggerEntitiesIndex;
-    private EntityQuery m_GroupShade;
     
+    private EntityQuery m_GroupShade;
 
-    struct ShadePair {
-        public Entity entity; // this entity lower height + translation.y
-        public float  shade;
-
-        // dev & testing only
-        public Entity entityB;
+    struct Genome {
+        public TxAutotrophChrome1AB txAutotrophChrome1Ab; 
+        public TxAutotrophColorGenome txAutotrophColorGenome; // this will be changed ColorChrome1AB
     }
     
     protected override void OnCreate() {
@@ -39,39 +34,21 @@ public class TriggerLeafSystem : JobComponentSystem {
 
         m_BuildPhysicsWorldSystem = World.GetOrCreateSystem<BuildPhysicsWorld>();
         m_StepPhysicsWorldSystem = World.GetOrCreateSystem<StepPhysicsWorld>();
-       
-
-        m_TriggerEntitiesIndex = new NativeArray<int>(1, Allocator.Persistent);
-        m_TriggerEntitiesIndex[0] = 0;
-
         m_GroupShade = GetEntityQuery(ComponentType.ReadWrite<Shade>());
-
-
-    }
-
-    protected override void OnDestroy() {
-        m_TriggerEntitiesIndex.Dispose();
         
     }
-    
-    struct GetTriggerEventCount : ITriggerEventsJob {
-        [NativeFixedLength(1)] public NativeArray<int> pCounter;
-        
-        // unsafe writing to this?
-        public void Execute(TriggerEvent triggerEvent)
-        {
-            pCounter[0]++;
-            
-        }
-    }
-    
-    struct MakeShadeDict : ITriggerEventsJob {
+
+    struct MakeTriggerDicts : ITriggerEventsJob {
         [ReadOnly]public ComponentDataFromEntity<Translation> translations; 
         [ReadOnly]public ComponentDataFromEntity<TxAutotrophPhenotype> txAutotrophPhenotype;
+        [ReadOnly]public ComponentDataFromEntity<TxAutotrophPollen> TxAutotrophPollen;
+        
+
         
         [ReadOnly]public NativeArray<Environment.EnvironmentSettings> environmentSettings;
         
         public NativeHashMap<Entity, float> shadeDict;
+        public NativeHashMap<Entity, Genome> fertilizeDict;
         
         // need entity a to be lower than entity B
         //  need leaf0 to be less than leaf1 
@@ -86,44 +63,49 @@ public class TriggerLeafSystem : JobComponentSystem {
             var eB = triggerEvent.Entities.EntityB;
             Entity e;
             Entity eOther;
-            
-            var swap = translations[eA].Value.y + txAutotrophPhenotype[eA].height> translations[eB].Value.y 
-                       + txAutotrophPhenotype[eB].height;
-            if (swap) {
-                e = eB;
-                eOther = eA;
-            }
-            else {
-                e = eA;
-                eOther = eB;
-            }
-            
-            var l0 = math.max(txAutotrophConsts.minShadeRadius,
-                         txAutotrophPhenotype[e].leaf)* txAutotrophConsts.leafShadeRadiusMultiplier;
-            var l1 = math.max(txAutotrophConsts.minShadeRadius,
-                         txAutotrophPhenotype[eOther].leaf)* txAutotrophConsts.leafShadeRadiusMultiplier;
 
-            var dSqr = math.distancesq(translations[e].Value, translations[eOther].Value);
-            var r0 = math.max(l0, l1);
-            var r1 = math.min(l0, l1);
-            var rSub = r0 - r1;
-            var minD = rSub * rSub;
-            var num = dSqr - minD;
-            if (!shadeDict.ContainsKey(e)) {
-                if(num <= 0 ) {
-                    shadeDict[e]= r1;
-                } else {
-                    var rAdd = r0 + r1;
-                    var maxD = rAdd * rAdd-minD;
-                    shadeDict[e]= (1-((maxD - num) / maxD)) *r1;
+            if (txAutotrophPhenotype.Exists(eA)) {
+                var swap = translations[eA].Value.y + txAutotrophPhenotype[eA].height > translations[eB].Value.y
+                           + txAutotrophPhenotype[eB].height;
+                if (swap) {
+                    e = eB;
+                    eOther = eA;
                 }
-            }
-            if(num <= 0 ) {
-                shadeDict[e]+= r1;
-            } else {
-                var rAdd = r0 + r1;
-                var maxD = rAdd * rAdd-minD;
-                shadeDict[e]+= (1-((maxD - num) / maxD)) *r1;
+                else {
+                    e = eA;
+                    eOther = eB;
+                }
+
+                var l0 = math.max(txAutotrophConsts.minShadeRadius,
+                             txAutotrophPhenotype[e].leaf) * txAutotrophConsts.leafShadeRadiusMultiplier;
+                var l1 = math.max(txAutotrophConsts.minShadeRadius,
+                             txAutotrophPhenotype[eOther].leaf) * txAutotrophConsts.leafShadeRadiusMultiplier;
+
+                var dSqr = math.distancesq(translations[e].Value, translations[eOther].Value);
+                var r0 = math.max(l0, l1);
+                var r1 = math.min(l0, l1);
+                var rSub = r0 - r1;
+                var minD = rSub * rSub;
+                var num = dSqr - minD;
+                if (!shadeDict.ContainsKey(e)) {
+                    if (num <= 0) {
+                        shadeDict[e] = r1;
+                    }
+                    else {
+                        var rAdd = r0 + r1;
+                        var maxD = rAdd * rAdd - minD;
+                        shadeDict[e] = (1 - ((maxD - num) / maxD)) * r1;
+                    }
+                }
+
+                if (num <= 0) {
+                    shadeDict[e] += r1;
+                }
+                else {
+                    var rAdd = r0 + r1;
+                    var maxD = rAdd * rAdd - minD;
+                    shadeDict[e] += (1 - ((maxD - num) / maxD)) * r1;
+                }
             }
         }
     }
@@ -143,32 +125,34 @@ public class TriggerLeafSystem : JobComponentSystem {
     
     protected override JobHandle OnUpdate(JobHandle inputDeps) {
         ComponentDataFromEntity<Translation> translations = GetComponentDataFromEntity<Translation>();
-        // Get the number of TriggerEvents so that we can allocate a native array
-        m_TriggerEntitiesIndex[0] = 0;
-        /*
-        JobHandle getTriggerEventCountJobHandle = new GetTriggerEventCount() {
-            pCounter = m_TriggerEntitiesIndex
-        }.Schedule(m_StepPhysicsWorldSystem.Simulation, ref m_BuildPhysicsWorldSystem.PhysicsWorld, inputDeps);
-         getTriggerEventCountJobHandle.Complete();
-        */
         
         var shades = GetEntityQuery(ComponentType.ReadOnly<Shade>())
             .ToEntityArray(Allocator.TempJob);
         var shadesCount = shades.Length;
         shades.Dispose();
         
-        //m_TriggerEntitiesIndex[0] is too large could use count of entites with triggers if can get it fast
-        var shadeDict = new NativeHashMap<Entity,float>(shadesCount, Allocator.TempJob);
-       // m_TriggerEntitiesIndex[0] = 0;
+        var unfertilizeds = GetEntityQuery(ComponentType.ReadOnly<Shade>())
+            .ToEntityArray(Allocator.TempJob);
+        var unfertilizedCount = unfertilizeds.Length;
+        unfertilizeds.Dispose();
         
-        JobHandle makeShadePairsJobHandle = new MakeShadeDict
+        var shadeDict = new NativeHashMap<Entity,float>(shadesCount, Allocator.TempJob);
+        var fertilizeDict = new NativeHashMap<Entity,Genome>(unfertilizedCount, Allocator.TempJob);
+        
+        JobHandle makeShadePairsJobHandle = new MakeTriggerDicts
         {
             translations = GetComponentDataFromEntity<Translation>(),
             txAutotrophPhenotype = GetComponentDataFromEntity<TxAutotrophPhenotype>(),
+            TxAutotrophPollen = GetComponentDataFromEntity<TxAutotrophPollen>(),
             environmentSettings = Environment.environmentSettings, 
-            shadeDict = shadeDict
+            shadeDict = shadeDict,
+            fertilizeDict = fertilizeDict
         }.Schedule(m_StepPhysicsWorldSystem.Simulation, ref m_BuildPhysicsWorldSystem.PhysicsWorld,  inputDeps);
-        makeShadePairsJobHandle.Complete();
+        
+        
+        
+        
+        
         
         
         
@@ -176,10 +160,15 @@ public class TriggerLeafSystem : JobComponentSystem {
         {
             shadeDict = shadeDict,
         }.Schedule(m_GroupShade, makeShadePairsJobHandle);
+        
+        
+        
+        
+        makeShadePairsJobHandle.Complete();
         addShadeJobHandle.Complete();
-        
         shadeDict.Dispose();
+        fertilizeDict.Dispose();
         
-        return makeShadePairsJobHandle;
+        return  addShadeJobHandle;
     }
 }
